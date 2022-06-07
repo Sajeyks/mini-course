45120ec78107174bb07ef093d63e4fa52345a760
Sajeyks <sajeyksmwangi@gmail.com>
2022-06-07 13:28:50 +0300

    csrf fix

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -130,3 +130,4 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication', )}
 
+CSRF_TRUSTED_ORIGINS = ['{{EDUCATIVE_LIVE_VM_URL}}']
