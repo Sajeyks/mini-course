40ef3a8a0e6bed0616fc733a2fc7f58fed4f56ae
Sajeyks <sajeyksmwangi@gmail.com>
2022-06-08 15:54:41 +0300

    login serializer initial commit

--- demo/main/serializers.py
+++ demo/main/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from django.contrib import auth
+from rest_framework.exceptions import AuthenticationFailed
 
 User = get_user_model()
 
@@ -27,3 +29,31 @@ class ResendVerificationEmailSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['email']
+
+
+class LoginSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(max_length=255, min_length=3)
+    password = serializers.CharField(max_length=68, min_length=8, write_only=True)
+    token = serializers.CharField(max_length=68, min_length=8, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'token']
+
+    def validate(self, attrs):
+        email = attrs.get('email', '')
+        password = attrs.get('password', '')
+
+        user = auth.authenticate(email=email, password=password)
+
+        if not user:
+            raise AuthenticationFailed('Invalid Login Credentials, try again!')
+        if not user.is_active:
+            raise AuthenticationFailed('Your Acccount is disabled, please contact admin')
+        if not user.is_verified:
+            raise AuthenticationFailed('Your Email is not verified')
+
+        return {
+            'email': user.email,
+            'tokens': user.get_tokens
+        }
