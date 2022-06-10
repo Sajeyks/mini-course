c1735fab6574137f469b1931c74ffa2f22cf6652
Sajeyks <sajeyksmwangi@gmail.com>
2022-06-10 22:55:01 +0300

    initial commit

--- demo/main/urls.py
+++ demo/main/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('register/', views.RegistrationView.as_view(), name="register"),
     path('verify-email/', views.EmailVerificationView.as_view(), name = "verify-email"),
-    path('resend-verification-email/', views.ResendVerificationEmailView.as_view(), name = "resend-verification-email"), # new
+    path('resend-verification-email/', views.ResendVerificationEmailView.as_view(), name = "resend-verification-email"),
+    path('login/', views.LoginView.as_view(), name="login") # new
  ]  
