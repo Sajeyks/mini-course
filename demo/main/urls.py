8c8ce9d40566fbc6b04d1356b9d8f9027f232df5
Sajeyks <sajeyksmwangi@gmail.com>
2022-06-29 15:21:38 +0300

    added refresh token

--- demo/main/urls.py
+++ demo/main/urls.py
@@ -1,9 +1,14 @@
 from .import views
 from django.urls import path
+from rest_framework_simplejwt.views import (   # new
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('register/', views.RegistrationView.as_view(), name="register"),
     path('verify-email/', views.EmailVerificationView.as_view(), name = "verify-email"),
     path('resend-verification-email/', views.ResendVerificationEmailView.as_view(), name = "resend-verification-email"),
-    path('login/', views.LoginView.as_view(), name="login") # new
+    path('login/', views.LoginView.as_view(), name="login"),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh') # new
+
  ]  
