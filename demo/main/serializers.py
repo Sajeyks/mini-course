f186542a8f07a1cccae1132fb999a903eb0cf660
Sajeyks <sajeyksmwangi@gmail.com>
2022-09-08 18:24:54 +0300

    add SetNewPasswordSerializer

--- demo/main/serializers.py
+++ demo/main/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.contrib import auth
 from rest_framework.exceptions import AuthenticationFailed
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.utils.encoding import force_str
+from django.utils.http import urlsafe_base64_decode
 
 User = get_user_model()
 
@@ -60,3 +63,27 @@ class LoginSerializer(serializers.ModelSerializer):
         
 class RequestPasswordResetEmailSerializer(serializers.Serializer):
     email = serializers.EmailField()
+    
+
+class SetNewPasswordSerializer(serializers.Serializer):
+    password = serializers.CharField(min_length=8, write_only=True)
+    token = serializers.CharField(write_only=True)
+    uidb64 = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        try:
+            password = attrs.get('password')
+            token = attrs.get('token')
+            uidb64 = attrs.get('uidb64')
+
+            id = force_str(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(id=id)
+
+            if not PasswordResetTokenGenerator().check_token(user, token):
+                raise AuthenticationFailed('The reset link is invalid or expired.', 401)
+            user.set_password(password)
+            user.save()
+
+        except Exception as e:
+            raise AuthenticationFailed('The reset link is invalid or expired.', 401)
+        return super().validate(attrs)
